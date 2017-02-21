3b9471a93cfdac8c20840cb4efc0785338b62d5e
dos533 <d.pereheyda@gmail.com>
2017-02-21 21:10:46 +0200

    First test

--- spec.js
+++ spec.js
@@ -1,5 +1,14 @@
-describe('', ()=> {
-    it('', ()=> {
-        expect(true).toBe(true)
+describe('test suite name', ()=> {
+    it('test case name', ()=> {
+        browser.get('')
+        let searchField = $("input[name = 'searchStr']")
+        let buttonSearch = $("span.input-group-btn")
+        let searchRequest = 'The Matrix'
+        searchField.sendKeys(searchRequest)
+        buttonSearch.click()
+        browser.sleep(5000)
+        let movieCard = $('movie-card')
+        let movieTitle = movieCard.$('h4 a').getText()
+        expect(movieTitle).toContain(searchRequest, 'First search result should contain The Matrix')
     })
 })
